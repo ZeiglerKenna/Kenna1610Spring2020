db2edf014e8076b90b395e9c1f8b2936f5f1f471
Kenna <zeiglken@gmail.com>
2020-01-24 21:25:44 -0700

    1/24/2020
    Tried a Unity tutorial, messed around with Rider functions

--- Project 1/Assets/Scripts/NewClass.cs
+++ Project 1/Assets/Scripts/NewClass.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 using UnityEngine.Events;
 
 public class NewClass : MonoBehaviour
@@ -10,8 +11,11 @@ public class NewClass : MonoBehaviour
     public int scoreValue;
     public float floatNumber;
     public UnityEvent uEvent;
-    
-
+    public GameObject sphere1;
+    public Animation sphere1Animation;
+    public string capsule;
+    public UnityEvent uAction;
+    public Graphics uAsset;
 
     // Start is called before the first frame update
     private void Start()
@@ -20,14 +24,26 @@ public class NewClass : MonoBehaviour
         print(intNum);
         print(floatNum);
         print(gameObj);
+        print(sphere1);
+        print(scoreValue);
+        print(floatNumber);
         uEvent.Invoke();
-        
+        print(sphere1Animation);
+        print(capsule);
+        if (Input.GetKeyDown(KeyCode.C))
+            GetComponent<Renderer> ().material.color = Color.cyan;
+        // Unity Tutorial: https://learn.unity.com/tutorial/scripts-as-behaviour-components?projectId=5c8920b4edbc2a113b6bc26a#5c8924ededbc2a113b6bc373
+        uAction.Invoke();
+        print(uAsset);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+      
+        {
+           
+        }
         
     }
 }
