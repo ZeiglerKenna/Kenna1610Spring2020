33db6977bb4054c26aec79b0ec127c0d11c40596
Kenna <zeiglken@gmail.com>
2020-01-28 20:54:10 -0700

    Practiced Trigger Events

--- Project 1/Assets/Scripts/TriggerEvents.cs
+++ Project 1/Assets/Scripts/TriggerEvents.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 using UnityEngine.Events;
 
 public class TriggerEvents : MonoBehaviour
@@ -8,5 +9,40 @@ public class TriggerEvents : MonoBehaviour
     private void OnTriggerEnter(Collider other)
     {
       triggerEnterEvent.Invoke();
+    }
+
+    public UnityEvent triggerExitEvent;
+
+    private void OnTriggerExit(Collider other)
+    {
+        triggerExitEvent.Invoke();
+    }
+
+    public UnityEvent triggerStayEvent;
+
+    private void OnTriggerStay(Collider other)
+    {
+        triggerStayEvent.Invoke();
+    }
+
+    public UnityEvent triggerEnterEventTwo;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        triggerEnterEventTwo.Invoke();
+    }
+
+    public UnityEvent triggerExitEventTwo;
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        triggerExitEventTwo.Invoke();
+    }
+
+    public UnityEvent triggerStayEventTwo;
+
+    private void OnTriggerStay2D(Collider2D other)
+    {
+        triggerStayEventTwo.Invoke();
     }
 }
