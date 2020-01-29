cc9e04144f4519cb198b6e26c576ef8a3493282d
Kenna <zeiglken@gmail.com>
2020-01-29 11:21:15 -0700

    1/29/2020

---
+++ Project 1/Assets/Scripts/MouseEvents.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class MouseEvents : MonoBehaviour
+{
+    
+    public UnityEvent onMouseDrag;
+    
+    private void OnMouseDrag()
+    {
+        onMouseDrag.Invoke();
+    }
+
+    public UnityEvent onMouseDown;
+    
+    private void OnMouseDown()
+    {
+        onMouseDown.Invoke();
+        }
+    
+    public UnityEvent onMouseUp;
+    
+    private void OnMouseUp()
+    {
+        onMouseUp.Invoke();
+            }
+
+    public UnityEvent  onMouseEnter, onMouseExit;
+
+    private void OnMouseEnter()
+    {
+        onMouseEnter.Invoke();
+            }
+    
+    private void OnMouseExit()
+    {
+        onMouseExit.Invoke();
+        }
+}
---
+++ Project 1/Assets/Scripts/TriggeringEvents.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class TriggeringEvents : MonoBehaviour
+{
+    public int intOne;
+    public float floatOne;
+    public string stringOne;
+    public UnityEvent uEventEnterOne;
+    public UnityEvent uEventExitOne;
+    public UnityEvent uEventStayOne;
+    private void OnTriggerEnter(Collider other)
+    {
+        print(intOne);
+        uEventEnterOne.Invoke();
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+       print(floatOne);
+       uEventExitOne.Invoke();
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+       uEventStayOne.Invoke();
+    }
+}
