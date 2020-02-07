2aee9d6f5a1d3da30211925e90701f47f2d25f0f
Kenna <zeiglken@gmail.com>
2020-02-06 20:37:53 -0700

    Practiced Controllers and Double Jump

---
+++ Project 1/Assets/Scripts/PracticeMoveAndDoubleJump.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class PracticeMoveAndDoubleJump : MonoBehaviour
+{
+    public CharacterController characterControl;
+    public Vector3 pos;
+    public float speed;
+    public float gravity;
+    public float jump;
+    public int maxCount;
+    private static double count;
+    void Update()
+    {
+        characterControl.Move(pos * Time.deltaTime);
+        pos.x = Input.GetAxis("Vertical") * speed;
+        pos.z = Input.GetAxis("Horizontal") * -speed;
+        pos.y -= gravity;
+
+        if (characterControl.isGrounded)
+        {
+            pos.y = 0;
+            count = 0;
+        }
+
+        if (Input.GetButtonDown("Jump") && count < maxCount)
+        {
+            pos.y = jump;
+            count++;
+        }
+    }
+}
