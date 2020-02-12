591f429822b1214323d22e521257ace12f3c0515
Kenna <zeiglken@gmail.com>
2020-02-11 23:05:50 -0700

    Practice If Statements

---
+++ Project 1/Assets/Scripts/HwIfStatements.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class HwIfStatements : MonoBehaviour
+{
+  public int a = 10;
+  public int b = 10;
+  public int c = 10;
+  public string passCode = "P@S$W0R4";
+  public bool canJump = true;
+  
+  void Start()
+  {
+    if (a + b <= c)
+    {
+      print("Big");
+    }
+    
+    if (passCode != "P@S$W0R4")
+    {
+      print("Wrong Password!");
+    }
+
+    if (canJump)
+    {
+      print("Can Jump!");
+    }
+    
+  }
+  
+} 
