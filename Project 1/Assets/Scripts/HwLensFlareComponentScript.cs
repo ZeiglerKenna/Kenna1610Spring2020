684784f02ca69a1db5009936973c65bd9668a947
Kenna <zeiglken@gmail.com>
2020-02-15 22:19:51 -0700

    Scripted to light components
    I set a toggle to the light to turn on and off and adjust intensity, and I also set the light to interpolate between two colors. I also tried a few things in another script for lens flare, but they didn't exactly work.

---
+++ Project 1/Assets/Scripts/HwLensFlareComponentScript.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using UnityEngine;
+
+[RequireComponent(typeof(LensFlare))]
+
+public class HwLensFlareComponentScript : MonoBehaviour
+{
+ private LensFlare lensFlare;
+ public float brightness = 10f;
+ public float fadeSpeed = 5f;
+
+ private void Start()
+ {
+  lensFlare = GetComponent<LensFlare>();
+ }
+
+ private void OnEnable()
+ {
+  brightness.Equals(20f);
+  fadeSpeed.Equals(10f);
+ }
+
+ private void OnDisable()
+ {
+  brightness.Equals(10f);
+  fadeSpeed.Equals(1f);
+ }
+}
---
+++ Project 1/Assets/Scripts/HwToggleLight.cs
@@ -0,0 +1,81 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Light))]
+
+public class HwToggleLight : MonoBehaviour
+{
+    private Light light;
+    public float intensity = 1f;
+    private float duration = 10f;
+    public Color color1 = Color.white;
+    public Color color2 = Color.red;
+
+    private void Start()
+    {
+        light = GetComponent<Light>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.Alpha1))
+        {
+            light.enabled = true;
+        }
+
+        if (Input.GetKeyUp(KeyCode.Alpha0))
+        {
+            light.enabled = false;
+        }
+
+        if (Input.GetKeyUp(KeyCode.L))
+        {
+            light.enabled = !light.enabled;
+        }
+
+        if (Input.GetKeyUp(KeyCode.Alpha2))
+        {
+            intensity = 2f;
+        }
+
+        if (Input.GetKeyUp(KeyCode.Alpha3))
+        {
+            intensity = 3f;
+        }
+        
+        if (Input.GetKeyUp(KeyCode.Alpha4))
+        {
+            intensity = 4f;
+        }
+        
+        if (Input.GetKeyUp(KeyCode.Alpha5))
+        {
+            intensity = 5f;
+        }
+        
+        if (Input.GetKeyUp(KeyCode.Alpha6))
+        {
+            intensity = 6f;
+        }
+        
+        if (Input.GetKeyUp(KeyCode.Alpha7))
+        {
+            intensity = 7f;
+        }
+        
+        if (Input.GetKeyUp(KeyCode.Alpha8))
+        {
+            intensity = 8f;
+        }
+        
+        if (Input.GetKeyUp(KeyCode.Alpha9))
+        {
+            intensity = 9f;
+        }
+
+        float time = Mathf.PingPong(Time.time, duration) / duration;
+        light.color = Color.Lerp(color1, color2, time);
+    }
+    
+    //Watched this video: https://www.youtube.com/watch?v=PCdg3cnQfZ4 and then played around with components to see what I could do
+    //Followed tutorial on interpolating light colors and then adjusted script to get a feel for it: https://docs.unity3d.com/ScriptReference/Light-color.html
+}
