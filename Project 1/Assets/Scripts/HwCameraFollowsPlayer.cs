0cbbb64dfe9ee4479604214126d5a5d74d276896
Kenna <zeiglken@gmail.com>
2020-02-16 16:37:55 -0700

    Scripted to Camera
    Followed a tutorial on having the camera follow the player, and then referenced the controller script we learned in class to see if I could figure out how to get the mouse to control the camera orientation. It works but is maybe not the most efficient way to do it.

---
+++ Project 1/Assets/Scripts/HwCameraFollowsPlayer.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class HwCameraFollowsPlayer : MonoBehaviour
+{
+    public GameObject player;
+    private Vector3 cameraOffset;
+    public float speed = 80f;
+
+    private void Start()
+    {
+        cameraOffset = transform.position - player.transform.position;
+    }
+
+    private void LateUpdate()
+    {
+        transform.position = player.transform.position + cameraOffset;
+    }
+    
+    //Tutorial: https://learn.unity.com/tutorial/movement-basics?projectId=5c514956edbc2a002069467c#5c7f8528edbc2a002053b711
+
+    private void Update()
+     {
+         var vector3 = new Vector3(Input.GetAxis("Mouse Y"), Input.GetAxis("Mouse X") * speed);
+         transform.Rotate(vector3 * (speed * Time.deltaTime));
+     }
+ }
