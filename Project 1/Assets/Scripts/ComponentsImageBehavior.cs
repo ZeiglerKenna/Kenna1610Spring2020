654e0a7507603b24f7ed5432c213d2c89f84dd82
Kenna <zeiglken@gmail.com>
2020-02-19 11:23:27 -0700

    2/19/2020

---
+++ Project 1/Assets/Scripts/ComponentsImageBehavior.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Image))]
+
+public class ComponentsImageBehavior : MonoBehaviour
+{
+    private Image imageObj;
+
+    private void Start()
+    {
+        imageObj = GetComponent<Image>();
+    }
+    
+    
+}
