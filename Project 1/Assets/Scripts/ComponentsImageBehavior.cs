1cdb193a23f1c4f18d1a5565a976cef496f44fd9
Kenna <zeiglken@gmail.com>
2020-02-20 23:17:52 -0700

    Tried to figure out health bar
    Struggling a little, I feel like I am starting to get the right idea, I just haven't gotten it to work yet, but I just want to push this before midnight anyway.

--- Project 1/Assets/Scripts/ComponentsImageBehavior.cs
+++ Project 1/Assets/Scripts/ComponentsImageBehavior.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 using UnityEngine.UI;
 
 [RequireComponent(typeof(Image))]
@@ -6,11 +7,15 @@ using UnityEngine.UI;
 public class ComponentsImageBehavior : MonoBehaviour
 {
     private Image imageObj;
+    public int currentHealth;
 
     private void Start()
     {
         imageObj = GetComponent<Image>();
     }
-    
-    
+
+    private void Update()
+    {
+        imageObj.fillAmount = currentHealth;
+    }
 }
