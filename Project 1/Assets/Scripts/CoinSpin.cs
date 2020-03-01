70e8a85ab27dda37ff9453d46f23bc40c43d2338
Kenna <zeiglken@gmail.com>
2020-02-29 23:29:08 -0700

    Level Build
    Started building a level in Unity to practice and apply what we have learned so far, I also wrote up a very simple script to get the coins to spin for extra flare

---
+++ Project 1/Assets/Scripts/CoinSpin.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class CoinSpin : MonoBehaviour
+{
+
+    private void Update()
+    {
+        transform.Rotate(2, 0, 0 * Time.deltaTime);
+    }
+}
---
+++ Project 1/Assets/Scripts/HwCameraFollowsPlayerOnlyNoMouse.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HwCameraFollowsPlayerOnlyNoMouse : MonoBehaviour
+{
+    public GameObject player;
+    private Vector3 cameraOffset;
+    public float speed = 80f;
+
+    private void Start()
+    {
+        cameraOffset = transform.position - player.transform.position;
+    }
+
+    private void LateUpdate()
+    {
+        transform.position = player.transform.position + cameraOffset;
+    }
+}
