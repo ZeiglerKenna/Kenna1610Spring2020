0f62ddb53adeefc4ca01feb36cae5a2b96c84f0f
Kenna <zeiglken@gmail.com>
2020-03-05 23:46:21 -0700

    3/5/2020

---
+++ Project 1/Assets/Scripts/Button.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Button))]
+
+public class Button : MonoBehaviour
+{
+   public Button uiButton;
+   private object onClick;
+
+   void Start()
+    {
+        Button btn = uiButton.GetComponent<Button>();
+       btn.onClick.AddListener(TaskOnClick);
+    }
+    
+    void TaskOnClick()
+    {
+        Debug.Log ("Congratulations!");
+    }
+    
+    //https://docs.unity3d.com/530/Documentation/ScriptReference/UI.Button-onClick.html
+    
+}
