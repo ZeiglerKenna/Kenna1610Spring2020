262fcb41b9c2fd6212eee8bb5574726e766820c4
Kenna <zeiglken@gmail.com>
2020-03-11 11:14:07 -0600

    3/11/2020

--- Project 1/Assets/Scripts/Countdown.cs
+++ Project 1/Assets/Scripts/Countdown.cs
@@ -8,13 +8,13 @@ public class Countdown : MonoBehaviour
     
     void Start()
     {
-        StartCoroutine(Countdown());
+      //  StartCoroutine(Countdown());
     }
 
-    IEnumerator Countdown()
-    {
-        WaitForSeconds(3);
-    }
+    //IEnumerator Countdown()
+    //{
+      //  WaitForSeconds(3);
+  //  }
     
     //Trying to figure out a countdown system using Coroutines referencing the Unity API for coroutines https://docs.unity3d.com/Manual/Coroutines.html
 }
