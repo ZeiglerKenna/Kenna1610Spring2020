33e103566db8dccbdeff0d003634b3ca16fe9745
Kenna <zeiglken@gmail.com>
2020-03-13 23:54:27 -0600

    Add and Remove Weapons Collectables

--- Project 1/Assets/Scripts/CollectableList.cs
+++ Project 1/Assets/Scripts/CollectableList.cs
@@ -2,10 +2,26 @@
 using UnityEngine;
 
 [CreateAssetMenu]
-
 public class CollectableList : ScriptableObject
 {
-   public List<Collectable> collectables;
    
+   public List<Collectable> collectables;
+   public string singleWeapon;
+
+   public void AddToList(Collectable weaponsObj)
+   {
+      collectables.Add(weaponsObj);
+   }
+
+   public void RemoveFromList (Collectable weaponsObj)
+   {
+      for (int i = 0; i < collectables.Count; i++)
+      {
+         if (collectables[i] == weaponsObj)
+         {
+            collectables.Remove(weaponsObj);
+         }
+      }
+   }
    
 }
