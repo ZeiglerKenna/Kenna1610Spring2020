814a067ce61928b966e55eb5e4fb92d5b89a1492
Kenna <zeiglken@gmail.com>
2020-03-25 11:21:36 -0600

    3/25/2020

---
+++ Project 1/Assets/Scripts/InClass/NavAgentBehavior.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class NavAgentBehavior : MonoBehaviour
+{
+   private NavMeshAgent agent;
+   public Transform player;
+   public float speed = 8f;
+   
+   private void Start()
+   {
+      agent = GetComponent<NavMeshAgent>();
+      agent.speed = speed;
+   }
+
+   private void Update()
+   {
+      agent.destination = player.position;
+   }
+}
