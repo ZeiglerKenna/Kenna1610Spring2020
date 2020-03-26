2c7d4d19faad7519960b5fb26c76c6a841a611d7
Kenna <zeiglken@gmail.com>
2020-03-26 17:31:05 -0600

    NavMesh Pactice
    Learned a method of creating a pathfinder using a Brackeys tutorial

---
+++ Project 1/Assets/Scripts/Homework/AiController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class AiController : MonoBehaviour
+{
+
+   public Camera mainCamera;
+   public NavMeshAgent agentAi;
+   public float speed = 50f;
+   
+   private void Update()
+   {
+      if (Input.GetMouseButtonDown(0))
+      {
+        Ray pathfinderRay = mainCamera.ScreenPointToRay(Input.mousePosition);
+        RaycastHit positionHit;
+
+        if (Physics.Raycast(pathfinderRay, out positionHit))
+        {
+            agentAi.SetDestination(positionHit.point);
+        }
+      }
+   }
+}
+
+//Tried Brackeys tutorials on NavMeshes like we mentioned in class to practice. I used this video: https://www.youtube.com/watch?v=CHV1ymlw-P8
