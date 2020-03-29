71d91fa12fcbf4304a6d813d50e6b25a78df41e3
Kenna <zeiglken@gmail.com>
2020-03-28 18:09:40 -0600

    3/28/2020
    I tried using a Brackeys tutorial on NavMeshes again, but it looks like this one has a lot of outdated functions that don't work in the new version of Unity and I noticed near the end of working on it so I figured I would just push what I did anyway and next week I will begin diving into my final game since we will be learning more about NavMeshes in class.

---
+++ Project 1/Assets/Scripts/Homework/LevelGenerater.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class LevelGenerater: MonoBehaviour
+{
+   public int width = 10;
+   public int height = 10;
+
+   public GameObject wallObj;
+   public GameObject playerAi;
+
+   private bool playerAiSpawned = false;
+
+   private void Start()
+   {
+      GenerateLevel();
+   }
+
+   void GenerateLevel()
+   {
+      for (int x = 0; x <= width; x += 2)
+      {
+         for (int y = 0; y <= height; y += 2)
+         {
+           if (Random.value > .7f)
+            {
+               Vector3 pos = new Vector3(x - width / 2f, 1f, y - height / 2f);
+               Instantiate(wallObj, pos, Quaternion.identity, transform);
+            } else if (!playerAiSpawned)
+            {
+               Vector3 pos = new Vector3(x - width / 2f, 1.25f, y - height / 2f);
+               Instantiate(playerAi, pos, Quaternion.identity);
+               playerAiSpawned = true;
+            }
+         }
+      }
+   }
+}
+
+//Used the second Brackeys Tutorial on Navmesh Pathfinders: https://www.youtube.com/watch?v=FkLJ45Pt-mY&list=PL41Evg_v_Nbas9qL1TJ3i_gifWhQr3Sfm&index=2&t=11s
+//He shows the code for the random generation of levels and I thought it would be fun to try so I also typed that out along with what he teaches in this tutorial
+//I actually believe that the new update of Unity got rid of some of the functions he uses in this tutorial, so I guess now this is just a level generation script
+//Looks like even the level generation works differently now
