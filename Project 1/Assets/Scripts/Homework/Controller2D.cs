62ef9375509f2234210f0ffcd0ea69e8a99dc15b
Kenna <zeiglken@gmail.com>
2020-04-04 22:02:53 -0600

    FinalGame4.4.2020
    Began my final game by importing my character sprites and getting controllers on the character and adding colliders and animations, etc. I still need to write my character controller 2D script to get the player moving but it's a good start so far.
    Watched through these Brackeys 2D tutorials to get started:
    https://www.youtube.com/watch?v=dwcT-Dch0bA&t=513s
    https://www.youtube.com/watch?v=hkaysu1Z-N8&t=251s

---
+++ Project 1/Assets/Scripts/Homework/Controller2D.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class Controller2D : MonoBehaviour
+{
+    public CharacterController2D controller2D;
+    private Vector3 positionDirection;
+    public float speed = 40f;
+    public float gravity = 0f;
+    public float jumpForce = 40f;
+    public Animator animator;
+    
+    void Update()
+    {
+        animator.SetFloat("Walk", Mathf.Abs(speed));
+        positionDirection.x = Input.GetAxis("Horizontal")*speed;
+    }
+}
--- Project 1/Assets/Scripts/InClass/Controller.cs
+++ Project 1/Assets/Scripts/InClass/Controller.cs
@@ -5,16 +5,14 @@ public class Controller : MonoBehaviour
 
     public CharacterController controller;
     private Vector3 positionDirection;
-    public float speed = 35f;
+    public float speed = 0f;
     public float gravity = -3f;
     public float jumpForce = 10f;
-
-    void Start()
-    {
-        
-    }
+    public Animator animator;
+    
     void Update()
     {
+        animator.SetFloat("Walk", Mathf.Abs(speed));
         positionDirection.x = Input.GetAxis("Horizontal")*speed;
         if (Input.GetButtonDown("Jump") && controller.isGrounded)
         {
