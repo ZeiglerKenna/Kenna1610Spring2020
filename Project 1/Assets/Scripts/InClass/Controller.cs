7a2def165d28654cefb87cbfde8dde250c135d5b
Kenna <zeiglken@gmail.com>
2020-04-06 11:20:41 -0600

    4.6.2020

--- Project 1/Assets/Scripts/InClass/Controller.cs
+++ Project 1/Assets/Scripts/InClass/Controller.cs
@@ -8,11 +8,9 @@ public class Controller : MonoBehaviour
     public float speed = 0f;
     public float gravity = -3f;
     public float jumpForce = 10f;
-    public Animator animator;
-    
+
     void Update()
     {
-        animator.SetFloat("Walk", Mathf.Abs(speed));
         positionDirection.x = Input.GetAxis("Horizontal")*speed;
         if (Input.GetButtonDown("Jump") && controller.isGrounded)
         {
@@ -20,5 +18,10 @@ public class Controller : MonoBehaviour
         }
         positionDirection.y += -gravity;
         controller.Move(positionDirection*Time.deltaTime);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+       
     }
 }
