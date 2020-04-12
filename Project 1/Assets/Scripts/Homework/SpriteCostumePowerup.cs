a00579b3e48bdb0f751bed7734842dff30b04bf3
Kenna <zeiglken@gmail.com>
2020-04-11 22:11:28 -0600

    Worked on Final Game
    Added a Powerup System. Need to work on changing sprite color after powerup later if there's time, otherwise just set up the UI (Health bar and money, etc.) and add more power ups/downs. Add enemies with navmesh. Figure out what's wrong with particles. Then all key components are there and I just need to build the level.

---
+++ Project 1/Assets/Scripts/Homework/SpriteCostumePowerup.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class SpriteCostumePowerup : MonoBehaviour
+{
+  public GameObject purplePowerUp;
+  public SpriteRenderer fireSprite;
+  public Sprite mushroomFireSprite;
+
+  private void Start()
+  {
+    fireSprite.GetComponent<SpriteRenderer>();
+  }
+
+  void OnTriggerEnter(Collider other)
+  {
+    if (purplePowerUp)
+    {
+      fireSprite.sprite = mushroomFireSprite;
+      print("Powerup!");
+    }
+  }
+}
